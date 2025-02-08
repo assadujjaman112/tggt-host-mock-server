72e371808b9469a7843e20a12fde944ec065a417
Assadujjaman Murad <assadujjamanmurad120@gmail.com>
2025-02-09 02:17:27 +0600

    first commit

--- api/server.ts
+++ api/server.ts
@@ -1,31 +1,30 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
+import serverless from "serverless-http";
 
+// Initialize Express
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Import JSON data
 import gameData from "../gameData.json";
 import templates from "../templates.json";
 import friends from "../friends.json";
 import notifications from "../notifications.json";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// API Routes
+app.get("/", (req, res) => {
+  res.send("Mock server is running");
+});
+//@ts-ignore
+app.get("/gameData", (req, res) => res.json(gameData));
+//@ts-ignore
+app.get("/templates", (req, res) => res.json(templates));
+//@ts-ignore
+app.get("/friends", (req, res) => res.json(friends));
+//@ts-ignore
+app.get("/notifications", (req, res) => res.json(notifications));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Mock server is running on Vercel (TypeScript)");
-});
-
-app.get("/gameData", (req: Request, res: Response) => {
-  res.json(gameData);
-});
-app.get("/templates", (req: Request, res: Response) => {
-  res.json(templates);
-});
-app.get("/friends", (req: Request, res: Response) => {
-  res.json(friends);
-});
-app.get("/notifications", (req: Request, res: Response) => {
-  res.json(notifications);
-});
-
-// Export the app for Vercel
-export default app;
+// Export as Serverless Function
+export default serverless(app);
